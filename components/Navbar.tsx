1ec757e7bf9afa578dfdfff3f4b1792162adbe37
Rasolomanana Olivier <herimanitraolivier@gmail.com>
2025-01-16 16:09:54 +0300

    feat: add CV download functionality to Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,12 +5,21 @@ import { Button } from "./ui/button";
 import { handleGetInTouch } from "@/utils/functions";
 
 export default function Navbar() {
+  const downloadCV = () => {
+    const link = document.createElement("a");
+    link.href = "/RASOLOMANANA_Herimanitra_Olivier.pdf";
+    link.download = "Olivier_CV.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <nav className="flex justify-between items-center py-5">
       <h1 className="text-2xl font-bold ml-2">Olivier.</h1>
 
       <div className="flex space-x-4">
-        <Button variant="outline" className="rounded-full">
+        <Button variant="outline" className="rounded-full" onClick={downloadCV}>
           Download CV
         </Button>
         <Button className="rounded-full" onClick={handleGetInTouch}>
